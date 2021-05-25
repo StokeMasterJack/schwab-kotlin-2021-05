9de6319223d710382bead03bb67fc7d210f8ea00
Dave Ford <dford@smart-soft.com>
2021-05-25 15:37:02 -0500

    StringBuildingLab - Problem Def

---
+++ p1/src/test/kotlin/_Labs/StringBuildingLab.kt
@@ -0,0 +1,28 @@
+package _Labs
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+fun computeCardName(valueName:String,suitName:String):String{
+
+
+    return "Ace of Spades"
+
+}
+
+//don't change the test
+//modify computeCardName function above to make the test pass
+//Checkmark when you are done
+class StringBuildingLab {
+
+
+    @Test
+    fun test() {
+        val cardName11:String = computeCardName("Ace","Spades")
+        val cardName44:String = computeCardName("4","Hearts")
+
+        assertEquals("Ace of Spades",cardName11)
+        assertEquals("4 of Hearts",cardName44)
+    }
+}
